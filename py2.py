d1ec28a8d84c11e448c1828dccc3d26827ab03b0
Prathamop156 <160272001+Prathamop156@users.noreply.github.com>
2024-02-17 18:15:57 +0530

    xnor

---
+++ py2.py
@@ -0,0 +1,18 @@
+name = "pratham"
+friend = "jack"
+print("enter your name")
+apple = """ He said,
+hi pratham
+hey i am good
+"I want to eat apple"""
+print("hello,"+ name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+for character in name:
+    print(character)
